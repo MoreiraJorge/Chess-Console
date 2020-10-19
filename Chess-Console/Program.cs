15ade344d22bcc46434ff5915c9f1a5a12e57837
MoreiraJorge <moreirajorge_17@outlook.pt>
2020-10-19 16:22:27 +0100

    Improve print board

--- Chess-Console/Program.cs
+++ Chess-Console/Program.cs
@@ -8,7 +8,7 @@ namespace Chess_Console
     {
         static void Main(string[] args)
         {
-            /*
+            
             try
             {
                 Board board = new Board(8, 8);
@@ -16,19 +16,15 @@ namespace Chess_Console
                 board.placeApiece(new Tower(board, Color.BLACK), new Position(0, 0));
                 board.placeApiece(new Tower(board, Color.BLACK), new Position(1, 3));
                 board.placeApiece(new King(board, Color.BLACK), new Position(2, 4));
+
+                board.placeApiece(new Tower(board, Color.WHITE), new Position(0, 5));
                 Window.printBoard(board);
 
             } catch (BoardException e)
             {
                 Console.WriteLine(e.Message);
             }
-            */
-
-            PositionChess pos = new PositionChess('c', 7);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosition());
-
-            Console.ReadLine();
+           
         }
     }
 }
--- Chess-Console/Window.cs
+++ Chess-Console/Window.cs
@@ -9,6 +9,7 @@ namespace Chess_Console
         {
             for(int i=0; i < board.lines; i++)
             {
+                Console.Write(8 - i + " ");
                 for(int j=0; j < board.columns; j++)
                 {
                     if (board.piece(i, j) == null)
@@ -17,12 +18,28 @@ namespace Chess_Console
                     }
                     else
                     {
-                        Console.Write(board.piece(i, j) + " ");
+                        printPiece(board.piece(i, j));
+                        Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
 
+        }
+
+        public static void printPiece(Piece piece)
+        {
+            if (piece.color == Color.WHITE)
+            {
+                Console.Write(piece);
+            } else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(piece);
+                Console.ForegroundColor = aux;
+            }
         }
     }
 }
